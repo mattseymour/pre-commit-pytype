fbe5302fb4c92f4a7af8e474a1abb19cda0d58eb
Matt Seymour <mattaseymour@gmail.com>
2020-10-21 10:42:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_dummy_package',
-    version='0.0.0',
-    install_requires=['pytype==2019.10.17'],
+    version='1.0.0',
+    install_requires=['pytype==2020.10.8'],
 )
