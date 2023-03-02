2342938916ed78a0cbe9936dc81b6ddd9190da18
Ricardo Zilleruelo <rzilleruelo@gmail.com>
2023-03-02 00:14:56 -0800

    Update pytype dependency

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_pytype',
-    version='1.2.0',
-    install_requires=['pytype==2023.1.31'],
+    version='1.3.0',
+    install_requires=['pytype==2023.2.17'],
 )
