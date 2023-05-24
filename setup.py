a9d3bd0ec881e30f1332c8a9e818b5181c322057
Matt Seymour <mattaseymour@gmail.com>
2023-05-24 16:43:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup
 
 setup(
     name='pre_commit_pytype',
-    version='1.1.0',
-    install_requires=['pytype==2022.4.26'],
+    version='1.2.0',
+    install_requires=['pytype==2023.05.08'],
 )
